cfaedde3c81805f81d6ed4498ac4236cc7bef7ec
Aleksandr <sir.zubareff2011@yandex.ru>
2021-10-24 13:51:40 +0300

    JS

---
+++ src/JS/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('ul.catalog_tabs').on('click', 'li:not(.catalog_tab_active)', function() {
+        $(this)
+          .addClass('catalog_tab_active').siblings().removeClass('catalog_tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+      $('ul.catalog-item_list').on('click', 'li:not(.catalog-item_it_active)', function() {
+        $(this)
+          .addClass('catalog-item_it_active').siblings().removeClass('catalog-item_it_active');
+          
+      });  
+});
+
+
+
